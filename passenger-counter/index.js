addd44889693b50514db17979646ac984119623c
Vadim <vadiminsk90@gmail.com>
2021-06-02 22:38:25 +0300

    add save button

--- passenger-counter/index.js
+++ passenger-counter/index.js
@@ -3,6 +3,8 @@
 const count = document.getElementById('count');
 const increment = document.getElementById('increment');
 const decrement = document.getElementById('decrement');
+const saveBtn = document.getElementById('save');
+const saveEl = document.getElementById('save-el');
 
 increment.addEventListener('click', function () {
   count.value = +count.value + 1;
@@ -18,4 +20,9 @@ decrement.addEventListener('click', function () {
   if (+count.value < 1) {
     decrement.setAttribute('disabled', '');
   }
+});
+
+saveBtn.addEventListener('click', function () {
+  let countStr = count.value + '' + '-';
+  saveEl.innerText += countStr;
 });
